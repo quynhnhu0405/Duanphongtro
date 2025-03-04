77e15eddc7568d9ff8f491f5b9b487b0e8b00e2d
quynhu0405 <145915754+quynhnhu0405@users.noreply.github.com>
2025-03-04 15:52:44 +0700

    develop introduce page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router'
 import DefaultLayout from './Layouts/DefaultLayout'
 import Home from './Pages/Home'
 import PostingRegulation from './Pages/PostingRegulations'
+import Introduce from './Pages/Introduce'
 function App() {
   return (
     <BrowserRouter>
@@ -11,7 +12,7 @@ function App() {
         <Route path="/" element={<DefaultLayout />} >
           <Route path="trang-chu" element={<Home/>} />
           <Route path="quy-dinh-dang-tin" element={<PostingRegulation/>} />
-          
+          <Route path="gioi-thieu" element={<Introduce/>} />
         </Route>
       </Routes>
     </BrowserRouter>
