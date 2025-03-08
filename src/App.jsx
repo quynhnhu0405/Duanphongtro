bc4e83d13badffe8a37d7192b09b3e75fb5e6c87
quynhu0405 <145915754+quynhnhu0405@users.noreply.github.com>
2025-03-08 15:45:44 +0700

    fix layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-<<<<<<< Updated upstream
 
 import "./App.scss";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router";
 import DefaultLayout from "./Layouts/DefaultLayout";
-import Home from "./Pages/Home";
 import PostingRegulation from "./Pages/PostingRegulations";
 import Introduce from "./Pages/Introduce";
 import ActiveRegulation from "./Pages/Regulations";
@@ -14,20 +12,10 @@ import Questions from "./Pages/Questions";
 import InstructPost from "./Pages/Instruct";
 import HomeLayout from "./Layouts/HomeLayout";
 import HomePage from "./Pages/Home";
-
-=======
-import './App.scss'
-import { BrowserRouter, Route, Routes } from 'react-router'
-import DefaultLayout from './Layouts/DefaultLayout'
-import Home from './Pages/Home'
-import PostingRegulation from './Pages/PostingRegulations'
-import Introduce from './Pages/Introduce'
->>>>>>> Stashed changes
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-<<<<<<< Updated upstream
         <Route element={<HomeLayout />}>
           <Route path="/" element={<HomePage />} />
         </Route>
@@ -39,12 +27,8 @@ function App() {
           <Route path="chinh-sach-bao-mat" element={<PrivacyPolicy />} />
           <Route path="cau-hoi-thuong-gap" element={<Questions />} />
           <Route path="huong-dan-dang-tin" element={<InstructPost />} />
-=======
-        <Route path="/" element={<DefaultLayout />} >
-          <Route path="trang-chu" element={<Home/>} />
           <Route path="quy-dinh-dang-tin" element={<PostingRegulation/>} />
           <Route path="gioi-thieu" element={<Introduce/>} />
->>>>>>> Stashed changes
         </Route>
       </Routes>
     </BrowserRouter>
