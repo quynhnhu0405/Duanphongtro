6c94210d2bab05b2f53f686b78f9f18648ed9d7c
quynhu0405 <145915754+quynhnhu0405@users.noreply.github.com>
2025-05-09 14:18:26 +0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    chunkSizeWarningLimit: 1600, // Tăng do sử dụng antd và recharts
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom', 'react-router'],
+          antd: ['antd', '@ant-design/icons'],
+          charts: ['recharts'],
+          vendor: ['axios', 'date-fns', 'slugify']
+        }
+      }
+    }
+  },
+  server: {
+    historyApiFallback: true
+  }
 })
